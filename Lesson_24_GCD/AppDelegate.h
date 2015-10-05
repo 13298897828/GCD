25be4adc81e9f0c306147a6f5e31a233da0c47e4
Tianqi Zhang <1120404010@qq.com>
2015-10-05 15:34:56 +0800

    Initial Commit

---
+++ Lesson_24_GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_24_GCD
+//
+//  Created by 张天琦 on 15/10/5.
+//  Copyright © 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_24_GCDTests/Lesson_24_GCDTests.m
@@ -0,0 +1,39 @@
+//
+//  Lesson_24_GCDTests.m
+//  Lesson_24_GCDTests
+//
+//  Created by 张天琦 on 15/10/5.
+//  Copyright © 2015年 张天琦. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Lesson_24_GCDTests : XCTestCase
+
+@end
+
+@implementation Lesson_24_GCDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
